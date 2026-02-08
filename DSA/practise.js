59a961df4792fe43cec41075c5e9cf92cc17b444
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-08 19:20:44 +0530

    implemented double linked list

--- DSA/practise.js
+++ DSA/practise.js
@@ -1,57 +1,49 @@
 class Node {
-    constructor ( value ) {
-        this.data = value;
+    constructor ( data ) {
+        this.data = data;
         this.next = null;
+        this.prev = null;
     };
 };
 
 
-class LinkedList {
+class DoubleLinkedList {
     constructor() {
         this.head = null;
+        this.tail = null;
     };
 
-
-    append( data ) {
+    append(data) {
         let newNode = new Node(data);
 
         if ( !this.head ) {
-            this.head = newNode;
-            return;
+            this.head = this.tail =  newNode;
         }else {
-
-
-              let current = this.head;
-              while ( current.next != null ) {
-                    current = current.next;
-              };
-
-              current.next = newNode;
-        }
+            this.tail.next = newNode;
+            newNode.prev = this.tail
+            this.tail = newNode;
+        } 
     };
-
 
 
     print() {
         if ( !this.head ) return;
 
-          let current = this.head;
-          let result = "";
-          while ( current ) {
-            result = result + current.data + "->";
-             current = current.next;
-          };
+        let current = this.head;
+        let result = "";
+        while ( current ) {
+              result = result + current.data + "< - > ";
+              current = current.next;
+        };
 
-          result = result + null;
-          console.log(result);
-    };
+        result = result + null;
+        return  console.log(result);
+    }
 };
 
-
-
-const list = new LinkedList();
+const list = new DoubleLinkedList();
 list.append(10);
-list.append(20)
+list.append(20);
 list.append(30);
 list.append(40);
 list.print();
