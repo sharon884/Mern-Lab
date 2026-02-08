d8516b911f15e06263dd1b33a5f077de9ab7b2a8
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-08 18:57:52 +0530

    implemeted single linked list

--- DSA/practise.js
+++ DSA/practise.js
@@ -1,3 +1,57 @@
-class Graph {
-    constructor 
-}
+class Node {
+    constructor ( value ) {
+        this.data = value;
+        this.next = null;
+    };
+};
+
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    };
+
+
+    append( data ) {
+        let newNode = new Node(data);
+
+        if ( !this.head ) {
+            this.head = newNode;
+            return;
+        }else {
+
+
+              let current = this.head;
+              while ( current.next != null ) {
+                    current = current.next;
+              };
+
+              current.next = newNode;
+        }
+    };
+
+
+
+    print() {
+        if ( !this.head ) return;
+
+          let current = this.head;
+          let result = "";
+          while ( current ) {
+            result = result + current.data + "->";
+             current = current.next;
+          };
+
+          result = result + null;
+          console.log(result);
+    };
+};
+
+
+
+const list = new LinkedList();
+list.append(10);
+list.append(20)
+list.append(30);
+list.append(40);
+list.print();
