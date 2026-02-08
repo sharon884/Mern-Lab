5549619ac11dd1df1385920d92e343ab28bae79d
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-08 21:10:55 +0530

    implemented a automatic timer app with using useEffect

--- React/src/App.js
+++ React/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
+import Timer from './Timer';
 
 function App() {
   return (
    <>
     <Counter/>
+    <Timer/>
    </>
   );
 }
--- React/src/Timer.jsx
+++ React/src/Timer.jsx
@@ -1,1 +1,35 @@
-import 
+import React from "react";
+import { useState,useEffect } from "react";
+
+
+function Timer () {
+
+const [ Time, setTime  ] = useState(0);
+
+
+   useEffect(()=> {
+
+     let interval = setInterval(()=> {
+        setTime(prev=> prev +1);
+     },1000);
+
+
+     return ()=> {
+     
+         clearInterval(interval);
+     } 
+
+   },[])
+
+    return (
+        <>
+        <h1> Timer :  {Time}</h1>
+        
+
+           {/* <button>Click</button> */}
+        </>
+    )
+};
+
+
+export default Timer; 
