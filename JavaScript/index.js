9967c365d74370403d149fc95cdba308bc81c7d0
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-08 14:25:19 +0530

    check palaindrome

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,20 +1,15 @@
-let num1 = 9;
-let num2 = 19;
-let nums = [ 3,4,1,3,1];
+let word = "malayaladadfam";
 
-const arrow = ( ...nums)=> {
+let word2 = "";
 
-    let sum = 0;
-
-    nums.reduce((a,b) => {
-        
-        sum =  sum + b;
-    },0)
+let reversed = word.split("").reverse().join("");
 
 
-     return sum;
-
+for ( let i = word.length -1 ; i >= 0; i-- ) {
+       word2 = word2 + word[i];
 };
 
+console.log(word2); 
 
-console.log(arrow(num1,num2,2,4,2,3,2,1,34));
+
+console.log(word == word2);
