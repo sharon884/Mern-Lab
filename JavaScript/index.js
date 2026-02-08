91e1946483c00e27711c799a58e7f14013b2a9ff
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-08 13:55:23 +0530

    arrow function to find sum of n numbers

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,12 +1,20 @@
 let num1 = 9;
 let num2 = 19;
+let nums = [ 3,4,1,3,1];
 
-const arrow = (num1,num2)=> {
-    let sum  = num1 + num2;
+const arrow = ( ...nums)=> {
+
+    let sum = 0;
+
+    nums.reduce((a,b) => {
+        
+        sum =  sum + b;
+    },0)
+
 
      return sum;
 
 };
 
 
-console.log(arrow(num1,num2));
+console.log(arrow(num1,num2,2,4,2,3,2,1,34));
