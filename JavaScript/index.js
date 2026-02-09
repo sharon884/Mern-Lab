690d5529bc533997150815d02c3486f620ed54d6
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-09 13:57:17 +0530

    Find the total salary of all employees.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,15 +1,14 @@
-let users = [
-  { name: "A", age: 20 },
-  { name: "B", age: 30 },
-  { name: "C", age: 25 },
-  { name: "D", age: 40 }
+let employees = [
+  { name: "A", salary: 2000 },
+  { name: "B", salary: 3000 },
+  { name: "C", salary: 4000 }
 ];
 
-let arr = [];
 
-for ( num of users ) {
-    console.log(num.age);
-    if ( num.age > 25 )  arr.push(num.name);
-};
+let a = employees.reduce((a,c) => {
+    a = a + c.salary;
 
-console.log(arr)
+    return a ;
+},0);
+
+console.log(a)
