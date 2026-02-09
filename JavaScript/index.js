ef388ed7e245da3b7e3349b36dc8160a417763b1
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-09 13:38:22 +0530

    two different approch for finding the frequency of array elements

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,26 +1,34 @@
-let arr = [1, 2, 2, 3, 4, 4, 5];
+let arr = ["apple", "banana", "apple", "orange", "banana", "apple"];
 
-let arr1 = [];
-// let c = new Set(arr);
-// console.log(c)
+let freq = {};
 
-// for ( num of arr ) {
-//     if ( !arr1.includes(num) ) {
-//         arr1.push(num);
-//     }
-// };
-
-
-// console.log(arr1);
-
-for ( let i = 0 ; i < arr.length-1; i++ ) {
-     for ( let j = i+1 ; j < arr.length-1; j++ ) {
-        if ( arr[i] === arr[j] ) {
-            arr.splice(j,1);
-        }
-     }
+for ( num of arr ) {
+    freq[num] = (freq[num] || 0 ) +1;
 };
 
+console.log(freq);
 
-console.log(arr);
 
+
+
+let f = {};
+for ( let i = 0 ;i < arr.length; i++ ) {
+    let count = 1;
+
+    if ( !f[arr[i]] ) {
+       
+          for ( let j = i+1; j < arr.length; j++ ) {
+        if ( arr[i] === arr[j] ) {
+             count++;
+        };
+
+
+        f[arr[i]] = count;
+    };
+
+    }
+  
+   
+};
+
+console.log(f);
