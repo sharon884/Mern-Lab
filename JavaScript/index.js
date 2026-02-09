1f320e4131596afaa140e020dc55127013e53f65
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-09 13:30:07 +0530

    remove duplicates using one place

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,11 +4,23 @@ let arr1 = [];
 // let c = new Set(arr);
 // console.log(c)
 
-for ( num of arr ) {
-    if ( !arr1.includes(num) ) {
-        arr1.push(num);
-    }
+// for ( num of arr ) {
+//     if ( !arr1.includes(num) ) {
+//         arr1.push(num);
+//     }
+// };
+
+
+// console.log(arr1);
+
+for ( let i = 0 ; i < arr.length-1; i++ ) {
+     for ( let j = i+1 ; j < arr.length-1; j++ ) {
+        if ( arr[i] === arr[j] ) {
+            arr.splice(j,1);
+        }
+     }
 };
 
 
-console.log(arr1);
+console.log(arr);
+
