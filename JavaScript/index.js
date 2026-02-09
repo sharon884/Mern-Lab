7f14da5897b11ba8daef9001cbd88340bb40d87b
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-09 13:20:27 +0530

    find sum of even numbers using reduce

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,15 +1,29 @@
 let arr = [1, 2, 3, 4, 5, 6];
 
-let c = arr.filter((a ) => a % 2 == 0 );
-console.log(c)
-function findSum ( arr ) {
-    let sum = 0;
+// let c = arr.filter((a ) => a % 2 == 0 );
+// console.log(c)
+// function findSum ( arr ) {
+//     let sum = 0;
 
-    for ( num of arr ) {
-        sum = sum + num;
-    };
+//     for ( num of arr ) {
+//         sum = sum + num;
+//     };
 
-    return sum ;
-};
+//     return sum ;
+// };
 
-console.log(findSum(c));
+// console.log(findSum(c));
+
+
+let sum  =  arr.reduce((a,n) => {
+
+     if ( n % 2 == 0 ) {
+        a = a + n;
+     };
+//  console.log(a);
+ return a;
+
+},0);
+
+
+console.log(sum)
