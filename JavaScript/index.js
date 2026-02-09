4dc39e9cf1c5c72c3a99a32ab065928e1f2fc48f
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-09 13:26:46 +0530

    remove duplicates from an array using for loop

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,14 @@
 let arr = [1, 2, 2, 3, 4, 4, 5];
 
-let c = new Set(arr);
-console.log(c)
+let arr1 = [];
+// let c = new Set(arr);
+// console.log(c)
+
+for ( num of arr ) {
+    if ( !arr1.includes(num) ) {
+        arr1.push(num);
+    }
+};
+
+
+console.log(arr1);
