9cdca1d4d2a2b2fe5f7d5b2274b29b8dc7f59541
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-10 17:25:44 +0530

    binary serach pracitse

--- DSA/practise.js
+++ DSA/practise.js
@@ -1,29 +1,27 @@
-let arr = [ 2,3,4,5,6,7,8];
-
-function binarySerach ( arr, target ) {
-
-     let left = 0; 
-     let right = arr.length-1;
-      
-     while ( left <= right ) {
-          
-        let mid = Math.floor(left+right/2);
-   
-           
-             if ( arr[mid] == target  ) return true;
-
-             if ( arr[ mid] >  target ) {
-                   right = mid - 1;
+let arr = [2, 4, 6, 8, 10]
+let  target = 5
 
 
-             }else if ( arr[mid] < target ) {
-                 left = mid + 1;
-             };
 
-     };
+function binarySearch ( arr, target ) {
+    let left = 0 ; 
+    let right = arr.length -1;
 
-      return false;
+    while ( left <= right ) {
+        let mid = Math.floor((left + right ) /2);
+
+        if ( arr[ mid] == target ) return mid;
+
+        if ( arr[mid] > target ) {
+            right = mid -1;
+
+        } else {
+            left = mid + 1;
+        }
+    };
+
+    return -1;
 };
 
 
-console.log(binarySerach(arr,2));
+console.log(binarySearch(arr,target));
