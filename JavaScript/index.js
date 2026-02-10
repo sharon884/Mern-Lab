3fcaf1322bad4eaf27a6ebf5ac1b40ad3231ab4b
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-10 15:51:45 +0530

    reverse an array without using built in method

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,14 +1,8 @@
+let arr =  [ 4, 3,6, 7,5,8];
 
+let arr1 = [];
+for ( let i = arr.length-1; i >= 0 ; i-- ) {
+    arr1.push(arr[i]);
+};
 
-function check ( num ) {
-    if ( num <= 1 ) return false;
-
-     for ( let i = 0 ; i < Math.sqrt(num); i++ ) {
-          if ( num % i == 0 ) return false;  
-     };
-
-     return true;
-}
-
-
-console.log(Math.sqrt(7));
+console.log(arr1);
