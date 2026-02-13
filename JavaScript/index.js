14b1106cb6e4ebfe0f1dacad77116d5be9fe5285
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-13 11:54:29 +0530

    example for the destructring

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,23 +1,4 @@
-let arr =  [ 2,2,2,4,5,22,44,22,44,6,7,8];
-
-let freq = {};
-let uniq = [];
-
-
-for ( num of arr ) {
-    freq[ num ] = ( freq[num] | 0 ) +1;
-
-};
-
-
-
-for ( key in freq ) {
-    console.log(freq[key]);
-
-    if ( freq[key] == 1 ) {
-        uniq.push(key);
-    };
-};
-
-
-console.log(uniq);
+let arr = [ 1,3,5,6,8];
+const [ one,two, three, four,five ] = arr;
+console.log(one);
+console.log(two);
