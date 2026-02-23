30b52b0504b9ca504d3ed6907523833c4e74a662
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-23 15:13:34 +0530

    second largest number

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,23 +1,44 @@
-let numbers = [3, 7, 2, 9, 4, 7, 2];
+// let numbers = [3, 7, 2, 9, 4, 7, 2];
 
 
-function doSome ( numbers )  {
+// function doSome ( numbers )  {
 
 
-for ( let i = 0 ; i < numbers.length ; i++ ) {
-        for ( let j = i+1; j < numbers.length ; j++ ) {
-               if ( numbers[i] == numbers[j] ) {
-                      numbers.splice(j,1);
-               }
-        }
+// for ( let i = 0 ; i < numbers.length ; i++ ) {
+//         for ( let j = i+1; j < numbers.length ; j++ ) {
+//                if ( numbers[i] == numbers[j] ) {
+//                       numbers.splice(j,1);
+//                }
+//         }
+// };
+
+
+// numbers.sort((a,b) => a -b );
+// return numbers;
+
+// }
+
+
+
+// console.log(doSome(numbers));
+let nums = [10, 5, 8, 20, 15];
+
+function find ( nums ) {
+         let first = -Infinity;
+         let second = -Infinity;
+
+       for ( let i = 0 ;  i < nums.length; i++ ) {
+          
+              if ( nums[i] >  first  ){ 
+                     first = nums[i];
+                   
+              } else if ( nums[i] < first && nums[i] > second ) {
+                      second = nums[i];
+              };
+
+       };
+
+       return second;
 };
 
-
-numbers.sort((a,b) => a -b );
-return numbers;
-
-}
-
-
-
-console.log(doSome(numbers));
+console.log(find( nums ));
