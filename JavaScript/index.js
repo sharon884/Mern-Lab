a9237fa30cd41f8883ff0c00343fe68a73f9385c
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-23 15:33:38 +0530

    stack implementation

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,44 +1,81 @@
-// let numbers = [3, 7, 2, 9, 4, 7, 2];
+// // let numbers = [3, 7, 2, 9, 4, 7, 2];
 
 
-// function doSome ( numbers )  {
+// // function doSome ( numbers )  {
 
 
-// for ( let i = 0 ; i < numbers.length ; i++ ) {
-//         for ( let j = i+1; j < numbers.length ; j++ ) {
-//                if ( numbers[i] == numbers[j] ) {
-//                       numbers.splice(j,1);
-//                }
-//         }
+// // for ( let i = 0 ; i < numbers.length ; i++ ) {
+// //         for ( let j = i+1; j < numbers.length ; j++ ) {
+// //                if ( numbers[i] == numbers[j] ) {
+// //                       numbers.splice(j,1);
+// //                }
+// //         }
+// // };
+
+
+// // numbers.sort((a,b) => a -b );
+// // return numbers;
+
+// // }
+
+
+
+// // console.log(doSome(numbers));
+// let nums = [10, 5, 8, 20, 15];
+
+// function find ( nums ) {
+//          let first = -Infinity;
+//          let second = -Infinity;
+
+//        for ( let i = 0 ;  i < nums.length; i++ ) {
+          
+//               if ( nums[i] >  first  ){ 
+//                      first = nums[i];
+                   
+//               } else if ( nums[i] < first && nums[i] > second ) {
+//                       second = nums[i];
+//               };
+
+//        };
+
+//        return second;
 // };
 
-
-// numbers.sort((a,b) => a -b );
-// return numbers;
-
-// }
+// console.log(find( nums ));
 
 
 
-// console.log(doSome(numbers));
-let nums = [10, 5, 8, 20, 15];
-
-function find ( nums ) {
-         let first = -Infinity;
-         let second = -Infinity;
-
-       for ( let i = 0 ;  i < nums.length; i++ ) {
-          
-              if ( nums[i] >  first  ){ 
-                     first = nums[i];
-                   
-              } else if ( nums[i] < first && nums[i] > second ) {
-                      second = nums[i];
-              };
-
+class Stack {
+       constructor () {
+               this.items = [];
+               
        };
 
-       return second;
-};
 
-console.log(find( nums ));
+       push ( value ) {
+            
+              this.items.push(value);
+       };
+
+
+       pop ( value ) {
+               this.items.pop();
+       };
+
+
+       isEmpty () {
+               return this.items.length === 0;
+       };
+
+       size () {
+               return this.items.length;
+       };
+
+       peek () {
+              return this.items[this.items.length-1];
+       };
+
+       print() {
+               return this.items;
+       }
+}
