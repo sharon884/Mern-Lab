bc8249b821303ff34ae24d7403cee2f96e1ee0f0
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-26 23:14:28 +0530

    exmaple for the file read and write operations

--- Node/server.js
+++ Node/server.js
@@ -1,6 +1,9 @@
+const { rejects } = require("assert");
+const { error } = require("console");
 const express = require("express");
 const app = express();
-const fs = require('fs')
+const fs = require('fs');
+const { resolve } = require("path");
 
 app.get("/",( req, res ) => { 
     return res.send("hai from express server");
@@ -9,18 +12,53 @@ app.get("/",( req, res ) => {
 
 
 
+// app.get("/read", ( req, res ) => {
+//      const read = new Promise(( resolve, reject ) => {
+          
+//            fs.readFile("file.text", "utf8" ,( error, data ) => {
+//              if ( error  ) {
+//                  reject(error);
+//              }else {
+//                 resolve( data);
+//              }
+//            })
+//      });
+
+//      read();
+// })
+
+
+
+
 app.get("/read", ( req, res ) => {
-     fs.readFile("file.text", "utf8", (err, data) => {
-         if ( err ) {
-            console.log(err);
-            return ;
+     fs.readFile("file.text","utf8", ( error , data ) => {
+          if ( error ) {
+             console.log( error);
+          }else {
+            console.log(data);
+          }
+     })
+});
+
+app.get("/write", ( req, res ) => {
+     fs.writeFile("file.text","hai ny name is sharon", ( error , data) => {
+         if ( error) {
+             console.log(error);
          }else {
-             console.log(data);
-             return ;
+             console.log("success");
          }
      })
 })
 
+app.get("/delete", ( req, res) => {
+      fs.unlink("file.text",( error , sucess) => {
+          if ( error) {
+             console.log(error);
+          }else {
+             console.log("success")
+          }
+      })
+});
 
 
 app.listen(5000, ( req, res ) => { 
