5e883c6ef91cafcf03b2ec4e47db98db77fc2167
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-26 22:46:55 +0530

    exmaple for path + query params sum

--- Node/server.js
+++ Node/server.js
@@ -2,21 +2,27 @@ const express = require("express");
 const app = express();
 
 
-app.use((req,res,next) => {
-     console.log(req.method);
-     if ( req.method =="GET" ) {
-        return res.send("blocked")
-     }else {
-        next();
-     }
-    //  next();
-});
+// app.use((req,res,next) => {
+//      console.log(req.method);
+//      if ( req.method =="GET" ) {
+//         return res.send("blocked")
+//      }else {
+//         next();
+//      }
+   
+// });
 
 
 app.get("/",( req, res ) => {
      return res.send("server is on ")
 });
 
+app.get("/sum/:num1", ( req, res ) => {
+     let num1 = req.params.num1;
+     let num2 = req.query.num2;
+     let sum = Number(num1) + Number(num2);
+     return res.send(sum);
+});
 
 
 
