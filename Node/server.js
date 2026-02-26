6dd22ab014ec1e59a23ceeb6fe6aecee7eb7f791
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-26 23:01:50 +0530

    example for the file read operation

--- Node/server.js
+++ Node/server.js
@@ -1,49 +1,28 @@
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
+const fs = require('fs')
 
-
-// // app.use((req,res,next) => {
-// //      console.log(req.method);
-// //      if ( req.method =="GET" ) {
-// //         return res.send("blocked")
-// //      }else {
-// //         next();
-// //      }
-   
-// // });
-
-
-// app.get("/",( req, res ) => {
-//      return res.send("server is on ")
-// });
-
-// app.get("/sum/:num1", ( req, res ) => {
-//      let num1 = req.params.num1;
-//      let num2 = req.query.num2;
-//      let sum = Number(num1) + Number(num2);
-//      return res.send(sum);
-// });
-
-
-
-// app.listen(5000, (req,res)=> {
-//     console.log("server listening on port http://localhost:5000")
-// })
-
-
-const http = require("http");
-
-
-
- const server = http.createServer((req, res) => {
-     if ( req.method === "GET" ) {
-          res.end("hellow hai from http server ");
-     }else {
-        res.end("hellow form http server");
-     }
+app.get("/",( req, res ) => { 
+    return res.send("hai from express server");
 });
 
 
-server.listen(5000,( req, res ) => {
-     console.log('server is listening on port http://localhost:5000')
+
+
+app.get("/read", ( req, res ) => {
+     fs.readFile("file.text", "utf8", (err, data) => {
+         if ( err ) {
+            console.log(err);
+            return ;
+         }else {
+             console.log(data);
+             return ;
+         }
+     })
+})
+
+
+
+app.listen(5000, ( req, res ) => { 
+    console.log(`server listening on port http://localhost:5000`);
 })
