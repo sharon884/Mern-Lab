b0c174b273ce451568fbb1ebb61e2793c626dacb
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-27 00:49:58 +0530

    implemented a timer app with start pause and reset options

--- React/src/Timer.jsx
+++ React/src/Timer.jsx
@@ -1,29 +1,52 @@
-import { useEffect, useState }  from "react";
+import {  useEffect, useState  } from "react";
 
 
 const Timer = () => {
-
-    const [ time , setTime ] = useState(0)
-     
-    useEffect(() => {
-   
-      const intervalId =     setInterval(() => {
-           setTime((prev) => prev +1 );
-        }, 1000);
-      
-
-        return () => {
-             clearInterval(intervalId)
-        }
-         
-    },[])
+     const [ time , setTime ] = useState(0);
+     const [ runnig, setRunning] = useState(false);
 
 
-    return (
-        <>
-         { time }
-        </>
-    )
+     useEffect(() => {
+
+        let intervalId ;
+          if (  runnig ) {
+              intervalId = setInterval(()=> {
+                 setTime((prev) => prev + 1);
+             },1000);
+          };
+
+          return () => {
+            clearInterval(intervalId)
+          }
+     },[runnig])
+
+
+const start = ()=> {
+     setRunning( true );
+};
+
+
+const pause = ()=> {
+     setRunning(false);
+
+};
+
+
+const reset = ()=> {
+     setTime(0);
+};
+
+
+ return (
+    <>
+    
+    <h1> { time} </h1>  
+     <button onClick={start}>start</button>
+     <button onClick={pause }>pause</button>
+     <button onClick={reset}>reset</button>
+    </>
+ )
+
 };
 
 
